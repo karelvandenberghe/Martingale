aad08f38086c9dde2cbe04165215be57c831d1ae
Karel Vandenberghe <karelvandenberghe84@gmail.com>
2021-09-29 08:35:36 +0200

    Make the program interactive

--- src/com/company/Martingale.java
+++ src/com/company/Martingale.java
@@ -20,9 +20,10 @@ public class Martingale {
             With every won play, the bet resets at € 10.
             The function will stop if:
             1. The goal of € 1200 is reached.
-            2. There is not enough money left to double the bet again. */
+            2. There is not enough money left to bet again. */
 
-        int money = 1000;
+        int money = 1000;  ;
+
         int target = 1200;
         int bet = 10;
             while (money >= bet) {
@@ -34,10 +35,8 @@ public class Martingale {
                 } else {
                     //50% chance for a "loss" on each play.
                     money -= bet;
-                    bet *= 2; /*if there was not enough money left to double, the function would stop
-                                here and the remaining balance would be shown.
-                                But if this is the case, we will reset the bet to €10
-                                in order to achieve all (€1200) or nothing (€0). */
+                    bet *= 2;
+
                     if(money <= bet){
                         bet = 10;
                     }
@@ -45,21 +44,33 @@ public class Martingale {
                 if (money >= target)
                     break;
             }
-            System.out.println(money); //Prints if we achieve €1200 or €0.
+            System.out.println(money); //Prints if we achieve all or nothing.
             return money;
             }
 
     public static void main(String[] args) {
 	// write your code here
-        int amountOfTries = 100000; //How many times do we want the function to run for statistics?
-        double numberOfWins = 0; //We will count how many times we achieve our goal of €1200.
-        for(int i = 1; i<=amountOfTries; i++) {
+        System.out.println("\nLet's calculate the odds to win money by playing a coin flipping betting game using the Martingale betting system. " +
+                "\nThe initial balance is 1000, the initial betting amount is 10 and the desired stopping amount is 1200." +
+                "\n" +
+                "\nThe rules are simple:" +
+                "\nIf you guess correct (heads or tails), you win the bet and start over with the initial betting amount." +
+                "\nIf you guess wrong, you will double the bet each time until you have won." +
+                "\nIf you do not have enough money left to double the bet, the betting amount will be reset to 10." +
+                "\nThe game ends when the desired amount of 1200 is obtained, or if you have lost all your money.");
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("How many times would you like the program to run for statistics? I suggest a minimum of 10000 times.");
+        String amountOfTries = scanner.nextLine();  // Read user input
+        int tries = Integer.valueOf(amountOfTries);
+
+        double numberOfWins = 0; //Count how many times we achieve our goal of €1200.
+        for(int i = 1; i<=tries; i++) {
             martingale();
-            if(martingale()==1200){
+            if(martingale() == 1200){
                 numberOfWins++;//increases numberOfWins each time our goal of €1200 is achieved.
             }
-        } //Time to calculate our chances to achieve our goal of € 1200!
-        System.out.println("There is a chance of " + numberOfWins/amountOfTries*100 + " % to reach the goal of € 1200.");
+        } //Calculate our chances to achieve all or nothing!
+        System.out.println("There is a chance of " + numberOfWins/tries*100 + " % to reach the goal of € 1200.");
     }
 }
 
